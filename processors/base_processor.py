33d16b4cee3263a2d57c68b4a2b95cbc9cd2c0ac
timf34 <farrelti@tcd.ie>
2024-10-13 23:39:39 +0100

    formatting and renaming

--- processors/base_processor.py
+++ processors/base_processor.py
@@ -1,18 +1,20 @@
-from abc import ABC, abstractmethod
 import pandas as pd
 
-from data_models import UnifiedArtworkData
+from abc import ABC, abstractmethod
+from typing import List
+
+from models.data_models import UnifiedArtwork
 
 
-class MuseumDataProcessor(ABC):
+class BaseMuseumDataProcessor(ABC):
     @abstractmethod
     def load_data(self, file_path: str) -> pd.DataFrame:
         pass
 
     @abstractmethod
-    def process_data(self, df: pd.DataFrame) -> list[UnifiedArtworkData]:
+    def process_data(self, df: pd.DataFrame) -> List[UnifiedArtwork]:
         pass
 
-    def get_unified_data(self, file_path: str) -> list[UnifiedArtworkData]:
+    def get_unified_data(self, file_path: str) -> List[UnifiedArtwork]:
         df = self.load_data(file_path)
         return self.process_data(df)
